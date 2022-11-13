2d74ccc81f9f8b02b0784bdf8449432073afe350
Gabriel <gabriel.gsm2017@gmail.com>
2022-11-13 15:28:44 -0300

    feat: Interface de altenticação pronta

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,9 +1,24 @@
-import {Text, Center } from "native-base";
+import {Text, Center, Icon } from "native-base";
+import Logo from '../assets/logo.svg';
+import { Button } from "../Components/Button";
+import { Fontisto } from '@expo/vector-icons';
 
 const Signin = () => {
   return(
-    <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24}>Signin</Text>
+    <Center flex={1} bgColor="gray.900" p={7}> 
+      <Logo width={212}  height={40}/>
+      <Button 
+        title="ENTRAR COM O GOOGLE"
+        leftIcon={<Icon as={Fontisto} name="google" color="#eee"size="md"/>}
+        type="SECONDARY"
+        mt={12}
+      />
+
+      <Text color="#eee" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'}
+        do seu e-mail com o google para criação da sua conta.
+      </Text>
+
     </Center>
   )
 }
