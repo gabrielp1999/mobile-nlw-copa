7844c320a26fc8764aab346ee0d70cf00aa54219
Gabriel <gabriel.gsm2017@gmail.com>
2022-11-13 13:49:36 -0300

    fix: correção node_modules

---
+++ src/screens/Signin.tsx
@@ -0,0 +1,11 @@
+import {Text, Center } from "native-base";
+
+const Signin = () => {
+  return(
+    <Center flex={1} bgColor="gray.900">
+      <Text color="white" fontSize={24}>Signin</Text>
+    </Center>
+  )
+}
+
+export default Signin;
