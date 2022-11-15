9d39a118ac0453ae4eda28e3abaa9bc1275d39c4
Gabriel <gabriel.gsm2017@gmail.com>
2022-11-15 11:20:10 -0300

    feat: Screen de Pools criada

--- App.tsx
+++ App.tsx
@@ -2,10 +2,12 @@
 import { NativeBaseProvider, StatusBar} from "native-base";
 import { THEME } from './src/styles/theme';
 import {useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from '@expo-google-fonts/roboto';
+
 import { Loading }from './src/Components/Loading';
+
 import { Signin } from "./src/screens/Signin";
 import { New } from './src/screens/New';
-import { Find } from './src/screens/Find';
+import { Pools } from './src/screens/Pools';
 
 
 import { AuthContextProvider } from './src/context/AuthContext';
@@ -22,7 +24,7 @@ import { AuthContextProvider } from './src/context/AuthContext';
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Find /> : <Loading /> }
+      { fontsLoaded ? <Pools /> : <Loading /> }
      </AuthContextProvider>
    </NativeBaseProvider> 
   );
